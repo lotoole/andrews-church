fc0afd2bd5a92e96129b4497c83962a1ddc7a62f
Liam T O'Toole <lotoole@uvm.edu>
2018-09-29 12:08:51 -0400

    finished first draft of the churches section

--- wp-content/themes/andrews/index.php
+++ wp-content/themes/andrews/index.php
@@ -4,6 +4,56 @@ get_header();
 
 ?>
 
-<h1>site is working</h1>
+<section class="churches">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-4">
+        <div class="church-info">
+          <img src="https://fillmurray.com/g/300/200" alt="">
+          <div class="content">
+            <h4>Saint Andrew Church</h4>
+            <p class="address">
+              109 South Main Street <br>
+              Route 100, Waterbury
+            </p>
+            <span style="font-weight: bold;">Weekend Masses</span>
+            <span>Date 1</span>
+            <span>Date 2</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="church-info">
+          <img src="https://fillmurray.com/g/300/200" alt="">
+          <div class="content">
+            <h4>Saint Andrew Church</h4>
+            <p class="address">
+              109 South Main Street <br>
+              Route 100, Waterbury
+            </p>
+            <span style="font-weight: bold;">Weekend Masses</span>
+            <span>Date 1</span>
+            <span>Date 2</span>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4">
+        <div class="church-info">
+          <img src="https://fillmurray.com/g/300/200" alt="">
+          <div class="content">
+            <h4>Saint Andrew Church</h4>
+            <p class="address">
+              109 South Main Street <br>
+              Route 100, Waterbury
+            </p>
+            <span style="font-weight: bold;">Weekend Masses</span>
+            <span>Date 1</span>
+            <span>Date 2</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
