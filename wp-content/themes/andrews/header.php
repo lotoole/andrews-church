c46c0c0ddd3b31577143b70b539b9a04ba1bca13
Liam T O'Toole <lotoole@uvm.edu>
2018-10-03 10:42:48 -0400

    fixed issue with mobile menu overlap, got rid of secondary menu that was just printing two menus

--- wp-content/themes/andrews/header.php
+++ wp-content/themes/andrews/header.php
@@ -19,7 +19,7 @@
         <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
         <!-- font-awesome -->
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-        
+
         <?php wp_head(); ?>
     </head>
     <body <?php body_class(); ?>>
@@ -37,7 +37,6 @@
             <nav class="mobile">
               <div class="mobile-track">
                 <?php wp_nav_menu( array( 'theme_location' => 'primary', 'container' => false ) ); ?>
-                <?php wp_nav_menu( array( 'theme_location' => 'secondary', 'container' => false ) ); ?>
               </div>
             </nav>
           <a href="#" class="mobile-nav-toggle"><i class="fa fa-bars" aria-hidden="true"></i></a>
