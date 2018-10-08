66e5759dbeb6057c5947bd4fe62d17fadb25199b
Liam T O'Toole <lotoole@uvm.edu>
2018-10-08 15:49:46 -0400

    finished bulletin page, added new hero for all pages

--- wp-content/themes/andrews/page-bulletin.php
+++ wp-content/themes/andrews/page-bulletin.php
@@ -6,14 +6,82 @@ get_header();
 
 <section class="single-heading">
   <div class="container">
-    <div class="bg"></div>
+    <?php $image = get_field('hero_image'); ?>
+    <div class="bg" style="background-image: url(<?php echo $image['url']; ?>);"></div>
     <div class="content">
-      <h1>Weekly Schedule</h1>
+      <h1><?php the_field('hero_text'); ?></h1>
     </div>
   </div>
 </section>
+
 <section class="schedule">
   <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Liturgy Schedule for This Week</h1><br>
+      </div>
+      <div class="col-md-12">
+        <table class="table table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Day</th>
+              <th scope="col">Month</th>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
+              <th scope="col">Church</th>
+              <th scope="col">Intention</th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php if( have_rows('bulletin') ): while ( have_rows('bulletin') ) : the_row(); ?>
+            <tr>
+              <th scope="row"><?php the_sub_field('day'); ?></th>
+              <td><?php the_sub_field('month'); ?></td>
+              <td><?php the_sub_field('date'); ?></td>
+              <td><?php the_sub_field('time'); ?></td>
+              <td><?php the_sub_field('church'); ?></td>
+              <td><?php the_sub_field('intention'); ?></td>
+            </tr>
+            <?php endwhile; endif; ?>
+          </tbody>
+        </table>
+    </div>
+    <div class="col-md-12">
+      <h1 style="margin-top: 40px;">Last Weekends Collections</h1>
+      <table class="table table-hover">
+        <thead class="thead-dark">
+          <tr>
+            <th scope="col">Parish</th>
+            <th scope="col">Offertory</th>
+            <th scope="col">Insurance</th>
+            <th scope="col">Fuel</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">St. Andrew Parish</th>
+            <td>$1761</td>
+            <td>$15</td>
+            <td>$357</td>
+          </tr>
+          <tr>
+            <th scope="row">Our Lady of the Snows/Saint Patrick Parish</th>
+            <td>$1177</td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <div class="col-md-12">
+      <h1 style="margin-top: 40px;">Contributing to Your Parish On-Line</h1>
+      <p>With assistance from the Diocese of Burlington, members of our parishes may now make weekly parish offertory contributions on-line via a <a href="https://www.myowngiving.com/Default.aspx?cid=540" target="_blank">secure website</a>. Since members of Our Lady of the Snows/St. Patrick Parish have expressed an interest in on-line giving, we are making this option available to them first.</p>
+    </div>
+
+
+
+
+    <!--
     <div class="row">
       <div class="col-md-12">
         <h1>Liturgy Schedule for This Week</h1><br>
@@ -98,34 +166,7 @@ get_header();
           </tbody>
         </table>
       </div>
-      <div class="col-md-12">
-        <h1 style="margin-top: 40px;">Last Weekends Collections</h1>
-        <table class="table table-hover">
-          <thead class="thead-dark">
-            <tr>
-              <th scope="col">Parish</th>
-              <th scope="col">Offertory</th>
-              <th scope="col">Insurance</th>
-              <th scope="col">Fuel</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">St. Andrew Parish</th>
-              <td>$1761</td>
-              <td>$15</td>
-              <td>$357</td>
-            </tr>
-            <tr>
-              <th scope="row">Our Lady of the Snows/Saint Patrick Parish</th>
-              <td>$1177</td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
-    </div>
+    </div> -->
   </div>
 </section>
 
