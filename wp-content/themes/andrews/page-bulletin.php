5b32750249d27dbb509fb3c102cf078caefd8e63
Liam T O'Toole <lotoole@uvm.edu>
2018-10-08 15:00:27 -0400

    added table to bulletin page, front end done

---
+++ wp-content/themes/andrews/page-bulletin.php
@@ -0,0 +1,97 @@
+<?php
+//Template Name: Bulletin
+get_header();
+
+?>
+
+<section class="schedule">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Liturgy Schedule for This Week</h1><br>
+      </div>
+      <div class="col-md-12">
+        <table class="table table-hover">
+          <thead class="thead-dark">
+            <tr>
+              <th scope="col">Day</th>
+              <th scope="col">Month</th>
+              <th scope="col">Date</th>
+              <th scope="col">Time</th>
+              <th scope="col">Church</th>
+              <th scope="col">Intention</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+            <tr>
+              <th scope="row">Tuesday</th>
+              <td>October</td>
+              <td>9</td>
+              <td>8:00 am</td>
+              <td>St. Andrew Church</td>
+              <td>Mass</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</section>
+
+<?php get_footer(); ?>
